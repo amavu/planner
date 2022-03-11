e97383dcc80fb633fb43e1403cb178a2b170e9f7
unknown <amandavu@gmail.com>
2022-03-11 16:41:24 +0100

    homepage linking and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>test</div>;
-}
+import React from "react";
+import "./App.scss";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Homepage } from "./components/Homepage";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Planner</h1>
+        <Switch>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
