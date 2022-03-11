6b4be86c4398b967a0ba3a58f5c486d1afb82e87
unknown <amandavu@gmail.com>
2022-03-11 20:49:15 +0100

    styled homepage view and added sign up link

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,31 @@
+import { ReactComponent as CalendarIcon } from "../icons/calendar-iso-gradient.svg";
+import { Link } from "react-router-dom";
+
 export const Homepage = () => {
   return (
-    <div>
+    <div className="homepage-container">
       <h2>Welcome to Planner!</h2>
+      <div className="register-text">
+        <p>NOT REGISTERED?</p>
+        <Link to={"/signup"}>CREATE A USER NOW!</Link>
+      </div>
+      <CalendarIcon width="300px" height="282px" />
+      <form>
+        <input
+          type="text"
+          placeholder="EMAIL"
+          className="input-style"
+          name="username"
+        />
+        <br></br>
+        <input
+          type="password"
+          placeholder="PASSWORD"
+          className="input-style"
+          name="username"
+        />
+      </form>
+      <button className="sign-in-button">SIGN IN</button>
     </div>
   );
 };
-
-export default Homepage;
